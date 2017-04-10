523d9a65e9dc1adeb925c6a51941b5464b7bc911
eduard.asatryan <ed.asatryan@gmail.com>
2017-04-11 01:47:24 +0400

    delete article

---
+++ app/assets/javascripts/article.js
@@ -0,0 +1,33 @@
+$('.article.show').ready(function () {
+
+});
+
+function addNewArticle() {
+    var title = $('#title').val();
+    var content = $('#content').val();
+    var video = $('#video').val();
+    var url = '/api/v1/articles' ;
+    $.ajax({
+        url: url,
+        method: 'POST',
+        contentType: 'application/json',
+        data:JSON.stringify({title:title,content:content,video:video})
+    }).done(function () {
+        // $button.closest('.weather-widget').remove();
+        // $.growl.notice({message: 'Forecast successfuly removed', location: 'bl'});
+    }).fail(function () {
+        // $.growl.warning({message: 'Forecast can\'t be removed', location: 'bl'});
+    })
+}
+
+function deleteArticle(id) {
+    var url = '/article';
+    $.ajax({
+        url: url,
+        method: 'DELETE',
+        contentType: 'application/json',
+        data:JSON.stringify({id:id}),
+        success: function(result) {
+        }
+    })
+}
