121e123c1e70375cec502e109a95cc9f6e442d56
David Shahbazyan <d.shahbazyan@gmail.com>
2017-04-11 02:23:05 +0400

    Migration file for Users' email column has been added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409212710) do
+ActiveRecord::Schema.define(version: 20170410221909) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                   collation: "nocase"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 20170409212710) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.string   "username"
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
